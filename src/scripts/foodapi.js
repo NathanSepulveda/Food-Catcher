c56615ced48b1f7c967105a35bd3ed5e99e8b83b
nathan sepulveda <nathans.composer@gmail.com>
2019-01-22 13:17:32 -0600

    fixed one error on 'Epices'

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -33,13 +33,18 @@ fetch("http://127.0.0.1:8088/foods?_sort=name&_order=desc")
             fetch(`https://world.openfoodfacts.org/api/v0/product/${food.barcode}.json`)
                 .then(response => response.json())
                 .then(productInfo => {
-                    // console.table(productInfo.product.ingredients)
+                    console.table(productInfo.product.ingredients)
                     food.ingredients = productInfo.product.ingredients.map(i => {                       
                         let stringI = ""
                         function toTitleCase(str) {
                             return str.replace(/\w\S*/g, function(txt){
-                                return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+                                if (txt.includes("poivron")) {
+                                    
+                                }
+                                const upperCased = txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+                                return upperCased
                             });
+                            
                         }
                         if (i.text.length >= 120) {                          
                             // console.log(i.text.split(" "))
@@ -47,12 +52,20 @@ fetch("http://127.0.0.1:8088/foods?_sort=name&_order=desc")
                             return stringI += `<li>${newArray[18]} ${newArray[19]}</li>`
                         } 
                         else if (i.text === "dont _gluten_" || i.text === "For allergens see ingredients in bold") {}
-                        else if (i.text.charAt(0) === "E" && isNaN(i.text.charAt(1)) === false) {}   
+                        else if (i.text.charAt(0) === "E" && isNaN(i.text.charAt(1)) === false) {}
+                        else if (i.text === "épices") {
+                            
+                            i.text = i.text.charAt(0).toUpperCase() + i.text.slice(1);
+                             
+                            stringI += `<li>${i.text}</li>`
+                            return stringI 
+                        }
                         
                         
                         else {
                             
                             i.text = i.text.split("_").join("")
+                            
                          
                             i.text = toTitleCase(i.text)
                        
@@ -69,17 +82,17 @@ fetch("http://127.0.0.1:8088/foods?_sort=name&_order=desc")
                     });
 
                     food.ingredients =  food.ingredients.join("")
-                    for (let i = 0; i < food.ingredients.length; i++) {
-                        const element = food.ingredients.charAt(i);
-                        const element2 = food.ingredients.charAt(i+1)
-                        let j = i
-                        if (element === "é" && element2 === "P") {
-                            // console.log("!!!!!!!", j)
-                            food.ingredients = food.ingredients.replace("P","p")
-                            food.ingredients = food.ingredients.replace("é","É")
-                            food.ingredients = food.ingredients.replace("P","p")
-                        }
-                    }
+                    // for (let i = 0; i < food.ingredients.length; i++) {
+                    //     const element = food.ingredients.charAt(i);
+                    //     const element2 = food.ingredients.charAt(i+1)
+                    //     let j = i
+                    //     if (element === "é" && element2 === "P") {
+                    //         // console.log("!!!!!!!", j)
+                    //         food.ingredients = food.ingredients.replace("P","p")
+                    //         food.ingredients = food.ingredients.replace("é","É")
+                    //         food.ingredients = food.ingredients.replace("P","p")
+                    //     }
+                    // }
 
                     
 
@@ -100,5 +113,7 @@ fetch("http://127.0.0.1:8088/foods?_sort=name&_order=desc")
                 })
         })
     })
+
+
 
 
